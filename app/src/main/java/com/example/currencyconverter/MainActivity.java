9ef56ba5d7b27b0050f20a8c0872442040f03a30
REZA <github email>
2021-09-29 10:24:24 +0330

    Initial commit

---
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.currencyconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Application;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText money;
+    Button convert;
+
+    Double amount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        money=(EditText) findViewById(R.id.money);
+        convert=(Button) findViewById(R.id.convert);
+        convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               amount=Double.parseDouble(money.getText().toString());
+                Toast.makeText(getApplicationContext(),String.valueOf(amount*2),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
